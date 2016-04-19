4a2c0c6aa594a543279eade956164a02a2bbc4ad
Christopher Foo <chris.foo@gmail.com>
2016-04-18 20:02:56 -0400

    Add https wildcard @match rule

--- header.js
+++ header.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         TPP Touchscreen Input Assist
 // @namespace    chfoo/tppinputassist
-// @version      1.3
+// @version      1.3.1
 // @homepage     https://github.com/chfoo/tppinputassist
 // @updateURL    https://raw.githubusercontent.com/chfoo/tppinputassist/master/tppinputassist.user.js
 // @description  Touchscreen coordinate tap overlay for inputting into Twitch chat
 // @author       Christopher Foo
-// @match        http://www.twitch.tv/*
+// @match        http://*.twitch.tv/*
+// @match        https://*.twitch.tv/*
 // @grant        none
 // ==/UserScript==
 /* jshint -W097 */
