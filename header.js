f4b6d2210a2f225621e40ac2735c05d6663c49a9
Christopher Foo <chris.foo@gmail.com>
2016-05-07 00:28:12 -0400

    header: Match only on naked subdomain or www

--- header.js
+++ header.js
@@ -6,8 +6,10 @@
 // @updateURL    https://raw.githubusercontent.com/chfoo/tppinputassist/master/tppinputassist.user.js
 // @description  Touchscreen coordinate tap overlay for inputting into Twitch chat
 // @author       Christopher Foo
-// @match        http://*.twitch.tv/*
-// @match        https://*.twitch.tv/*
+// @match        http://twitch.tv/*
+// @match        http://www.twitch.tv/*
+// @match        https://twitch.tv/*
+// @match        https://www.twitch.tv/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-2.2.3.min.js
 // @require      https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
