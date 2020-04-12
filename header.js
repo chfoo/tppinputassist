3a086bf6d4fbba72a9393c4d234d5462b5df68e8
Christopher Foo <chris.foo@gmail.com>
2020-04-12 16:54:28 -0400

    Remove @updateURL to not override alternate hosted version.
    It wasn't even being used correctly. Blame the original Userscripts
    website tutorial.

--- header.js
+++ header.js
@@ -3,7 +3,6 @@
 // @namespace    chfoo/tppinputassist
 // @version      1.11.0
 // @homepage     https://github.com/chfoo/tppinputassist
-// @updateURL    https://raw.githubusercontent.com/chfoo/tppinputassist/master/tppinputassist.user.js
 // @description  Touchscreen coordinate tap overlay for inputting into Twitch chat
 // @author       Christopher Foo
 // @match        http://twitch.tv/*
